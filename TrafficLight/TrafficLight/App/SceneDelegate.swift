bb5344625df003d437528927777d13b0dd3a97e5
Matvei <khlestov.matvei@mail.ru>
2024-03-27 12:39:57 +0500

    Created base project, removed storyboard references

---
+++ TrafficLight/TrafficLight/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  TrafficLight
+//
+//  Created by Matvei Khlestov on 27.03.2024.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
